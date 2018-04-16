8c12c988c9a242a25ecd0f1f1b8dc2e774f51016
Luffy <bibolost1@gmail.com>
2018-04-16 03:02:03 +0200

    transaction

--- app/src/main/java/com/example/android/blndin/Fragments/NewsfeedFragment.java
+++ app/src/main/java/com/example/android/blndin/Fragments/NewsfeedFragment.java
@@ -3,6 +3,7 @@ package com.example.android.blndin.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.view.ViewGroup;
 import com.example.android.blndin.Adapters.NewsfeedAdapter;
 import com.example.android.blndin.Models.NewsfeedModel;
 import com.example.android.blndin.R;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,17 +24,16 @@ public class NewsfeedFragment extends Fragment {
     RecyclerView recyclerView;
     RecyclerView.Adapter adapter;
     ArrayList<NewsfeedModel> models;
-    public NewsfeedFragment() {
-        // Required empty public constructor
-    }
-
-
+    LinearLayoutManager layoutManager;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v=inflater.inflate(R.layout.fragment_newsfeed, container, false);
         recyclerView=(RecyclerView)v.findViewById(R.id.recycler_newsfeed);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
         models=new ArrayList<>();
         for(int i=0;i<5;i++)
             models.add(new NewsfeedModel("Mostafa Bder",true));
--- app/src/main/java/com/example/android/blndin/ParentActivity.java
+++ app/src/main/java/com/example/android/blndin/ParentActivity.java
@@ -1,13 +1,50 @@
 package com.example.android.blndin;
 
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationMenu;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class ParentActivity extends AppCompatActivity {
+import com.example.android.blndin.Fragments.NewsfeedFragment;
 
+public class ParentActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView navigation;
+    FragmentManager manager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent);
+         navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+       manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.container, new NewsfeedFragment()).commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_wall:
+                FragmentManager manager = getSupportFragmentManager();
+                manager.beginTransaction().replace(R.id.container, new NewsfeedFragment()).commit();
+                break;
+            case R.id.nav_go:
+
+                break;
+
+            case R.id.nav_mysquads:
+
+                break;
+            case R.id.nav_hangouts:
+
+                break;
+            case R.id.nav_extras:
+
+                break;
+        }
+        return true;
     }
 }
