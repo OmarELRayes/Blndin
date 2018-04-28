ee7300146a4871bccbe94c8c6ba972b7fa18e7fc
Luffy <bibolost1@gmail.com>
2018-04-28 02:55:17 +0200

    notification

---
+++ app/src/main/java/com/example/android/blndin/Adapters/NotificationAdapter.java
@@ -0,0 +1,37 @@
+package com.example.android.blndin.Adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.blndin.R;
+
+/**
+ * Created by Luffy on 4/28/2018.
+ */
+
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.ViewHolder> {
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_notification,parent,false);
+        return new NotificationAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
